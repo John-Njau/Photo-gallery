f0b19181ae152758c353375574ced103c49861d5
john.njau <john.njoroge@student.moringaschool.com>
2022-05-29 21:40:04 +0300

    feature: search url path

--- photos/urls.py
+++ photos/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('',views.navbar, name='navbar'),
     re_path(r'^photos/$', views.allphotos, name='allphotos'),
+    re_path(r'^search/', views.search_results, name='search_results'),
     path('categoryview/<int:categoryId>', views.categoryview, name='categoryview'),
     path('locationview/<int:locationId>', views.locationview, name='locationview'),
-]
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
