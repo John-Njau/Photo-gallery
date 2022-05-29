f9b6aefd30f108f7fd0b76a2d263c1a4bdb6d87b
john.njau <john.njoroge@student.moringaschool.com>
2022-05-29 13:15:12 +0300

    feature: Added views to handle data display

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
-
+from .models import Category, Image, Location
 # Create your views here.
 def home(request):
-    return render(request, 'display/home.html')
+    details = Category.objects.all()
+    return render(request, 'display/home.html', {'details': details})
+
+def navbar(request):
+    details = Category.objects.all()
+    locations = Location.objects.all()
+    return render(request, 'display/navbar.html', {'details': details, 'locations': locations})
+
 
 def allphotos(request):
-    return render(request, 'display/photos.html')
+    photos = Image.objects.all()
+    category = Category.objects.all()
+    return render(request, 'display/photos.html',{'photos':photos, 'category':category})
+
+def image_modal(request, categoryId):
+    images = Image.objects.get(id=categoryId)
+    return render(request, 'display/home.html', {'images': images})
+    
+    
+def categoryview(request, categoryId):
+    category = Category.objects.get(id=categoryId)
+    images = Image.objects.get(id=categoryId)
+    title = f'{categoryId} category View'
+    msg = f'You are accessing category {categoryId}'
+    return render(request, 'display/categoryview.html', {'images': images, 'title': title, 'msg': msg, 'category': category})
+
+def locationview(request, locationId):
+    location =Location.objects.get(id=locationId)
+    images = Image.objects.get(id=locationId)
+    return render(request, 'display/locationview.html', {'images': images, 'location': location})    
