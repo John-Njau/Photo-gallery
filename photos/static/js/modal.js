abe19a411d6c10c017f4abdcfc1a69b2d8bc8e47
john.njau <john.njoroge@student.moringaschool.com>
2022-05-30 01:01:28 +0300

    feature: copy image url functionality

--- photos/static/js/modal.js
+++ photos/static/js/modal.js
@@ -1,9 +1,25 @@
 function copyUrl(url) {
-    function copy(event){
+    function handler(event){
         event.clipboardData = setData('text/plain', url);
         event.preventDefault();
-        document.removeEventListener('copy', copy, true);
+        document.removeEventListener('copy', handler, true);
     }
-    document.addEventListener('copy', copy, true);
+    document.addEventListener('copy', handler, true);
     document.execCommand('copy');
 }
+
+// let toCopyBtn = document.querySelector('.toCopy')
+
+// toCopyBtn.addEventListener('click', function(event) {
+//     var copyText = document.querySelector('.toBeCopied');
+//     copyText.focus();
+//     copyText.select();
+  
+//     try {
+//       var successful = document.execCommand('copy');
+//       var msg = successful ? 'successful' : 'unsuccessful';
+//       console.log('Copying text command was ' + msg);
+//     } catch (err) {
+//       console.log('Oops, unable to copy');
+//     }
+//   });
