0846bcde81d5db524a32e5e31ba8d639f4e38f8a
john.njau <john.njoroge@student.moringaschool.com>
2022-05-29 13:13:50 +0300

    feature: added category and location urls

--- photos/urls.py
+++ photos/urls.py
@@ -1,8 +1,15 @@
-import re
 from django.urls import re_path, path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home, name='home'),
-    re_path(r'^photos/$', views.allphotos, name='photos'),
+    path('',views.navbar, name='navbar'),
+    re_path(r'^photos/$', views.allphotos, name='allphotos'),
+    path('categoryview/<int:categoryId>', views.categoryview, name='categoryview'),
+    path('locationview/<int:locationId>', views.locationview, name='locationview'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
