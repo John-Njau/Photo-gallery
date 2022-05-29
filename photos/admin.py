1c13b6d86acae5844b1e35703c17c75e28fea061
john.njau <john.njoroge@student.moringaschool.com>
2022-05-29 13:12:20 +0300

    feature: registering models to the admin side

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Image, Location
 
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('category',)
+
+admin.site.register(Category)
+admin.site.register(Image,ImageAdmin)
+admin.site.register(Location)
