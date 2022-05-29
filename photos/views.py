758b5a1369318a6890e54042df810ebca8bc8466
john.njau <john.njoroge@student.moringaschool.com>
2022-05-29 21:39:20 +0300

    feature: implemented search by photo name functionality

--- photos/views.py
+++ photos/views.py
@@ -4,7 +4,8 @@ from .models import Category, Image, Location
 # Create your views here.
 def home(request):
     details = Category.objects.all()
-    return render(request, 'display/home.html', {'details': details})
+    locations = Location.objects.all()
+    return render(request, 'display/home.html', {'details': details, 'locations': locations})
 
 def navbar(request):
     details = Category.objects.all()
@@ -16,47 +17,28 @@ def allphotos(request):
     print(photos)
     return render(request, 'display/photos.html',{'photos':photos})
 
-# def image_modal(request, categoryId):
-#     images = Image.objects.get(id=categoryId)
-#     return render(request, 'display/home.html', {'images': images})
+def categoryview(request, categoryId):
+    ind_category = Category.objects.get(pk=categoryId)
+    photos = Image.objects.filter(category=ind_category)
+    print(photos)
+    title = f'Photos in {ind_category} category'
+    photocount = photos.count()
+    msg = f'There are {photocount} photos in this category'
+    return render(request, 'category/categoryview.html', {'photos': photos, 'title': title, 'msg': msg, 'category': ind_category})
 
 def locationview(request, locationId):
-    location =Location.objects.get(id=locationId)
-    images = Image.objects.get(id=locationId)
-    return render(request, 'display/locationview.html', {'images': images, 'location': location})    
+    ind_location =Location.objects.get(id=locationId)
+    photos = Image.objects.filter(location=ind_location)
+    photocount =photos.count()
+    msg = f'There are {photocount} photos shot in this location'
+    return render(request, 'location/locationview.html', {'photos': photos, 'location': ind_location, 'msg': msg})  
 
-def beaches(request):
-    cat_beaches = Category.objects.get(pk=3)
-    beaches = Image.objects.filter(category=cat_beaches)
-    return render(request,'category/beaches.html', {'beaches':beaches})
-
-def nature(request):
-    cat_nature = Category.objects.get(pk=1)
-    nature = Image.objects.filter(category=cat_nature)
-    return render(request,'category/nature.html', {'nature':nature})
-   
-
-def animals(request):
-    cat_animals = Category.objects.get(pk=4)
-    animals = Image.objects.filter(category=cat_animals)
-    return render(request,'category/animals.html', {'animals':animals})
-
-
-def food(request):
-    cat_food = Category.objects.get(pk=4)
-    food = Image.objects.filter(category=cat_food)
-    return render(request,'category/food.html', {'food':food})
-
-def technology(request):
-    pass
-
-def paintings(request):
-    pass
-    
-# def categoryview(request):
-#     ind_category = Category.objects.get(pk=3)
-#     images = Image.objects.filter(category=ind_category)
-#     print(images)
-#     title = f'{ind_category} category View'
-#     msg = f'You are accessing category {ind_category}'
-#     return render(request, 'display/categoryview.html', {'images': images, 'title': title, 'msg': msg})
+def search_results(request):
+    if 'term' in request.GET and request.GET["term"]:
+        search_term = request.GET.get("term")
+        searched_terms = Image.search_by_name(search_term)
+        message = f"{search_term}"
+        return render(request, 'search/search.html',{"message":message,"terms": searched_terms})
+    else:
+        message = "Searched term not found"
+        return render(request, 'search/search.html', {"message":message})
